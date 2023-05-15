9aeecfcdfe3f95f1b6908e9cccf800dafa5f88c0
SergiiMal <ser4ester@gmail.com>
2023-05-15 22:58:34 +0300

    +

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+import { selectError } from 'redux/selector';
+import { useSelector } from 'react-redux';
+import { Container, Title, TitleContacts } from './App.styled';
+import { errorServes } from './ErrorPage/error';
+
+function App() {
+  const error = useSelector(selectError);
+  return (
+    <Container>
+      {error && errorServes()}
+      <Title>PhoneBook</Title>
+      <ContactForm />
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter />
+      <ContactsList />
+    </Container>
+  );
+}
+
+export default App;
